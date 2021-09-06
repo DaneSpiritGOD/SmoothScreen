89bed3ce8fa9646b06a0ca7d5bba61a098c2f41c
dane.dai <Dane.dai@wisetechglobal.com>
2021-09-06 22:26:15 +0800

    add reference

--- SmoothScreen/Program.cs
+++ SmoothScreen/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+using Gma.System.MouseKeyHook;
 
 namespace SmoothScreen
 {
@@ -6,7 +10,17 @@ namespace SmoothScreen
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Hook.GlobalEvents().MouseMoveExt += Program_MouseMove;
+			Application.Run();
+		}
+
+		private static void Program_MouseMove(object sender, MouseEventArgs e)
+		{
+			Debug.WriteLine(e.Location);
+			if (e.X > 1000 && e.X < 1500)
+			{
+				Cursor.Position = new Point(e.X + 10, e.Y);
+			}
 		}
 	}
 }
