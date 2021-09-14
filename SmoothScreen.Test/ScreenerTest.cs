09f4ab805f768d71cecd5103ee1a44379c1455d7
dane.dai <Dane.dai@wisetechglobal.com>
2021-09-14 20:50:25 +0800

    refactor border

--- SmoothScreen.Test/ScreenerTest.cs
+++ SmoothScreen.Test/ScreenerTest.cs
@@ -1,119 +1,113 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using NUnit.Framework;
 
 namespace SmoothScreen.Test
 {
 	class ScreenerTest
 	{
-		[TestCase(0, 0, Border.Left)]
-		[TestCase(4, 0, Border.Top)]
-		[TestCase(0, 4, Border.Left)]
-		[TestCase(4, 4, Border.Left)]
-		[TestCase(1, 3, Border.Left)]
-		[TestCase(3, 1, Border.Top)]
-		[TestCase(3, 3, Border.Left)]
-		public void TestGetCloserBorder_TopLeft(int x, int y, Border expectedBorder)
+		[TestCase(0, 0, "Left")]
+		[TestCase(4, 0, "Top")]
+		[TestCase(0, 4, "Left")]
+		[TestCase(4, 4, "Left")]
+		[TestCase(1, 3, "Left")]
+		[TestCase(3, 1, "Top")]
+		[TestCase(3, 3, "Left")]
+		public void TestGetCloserBorder_TopLeft(int x, int y, string expectedBorder)
 		{
-			var point = new Point(x, y);
-			Assert.That(screener.topLeftRect.Contains(point), Is.True);
-			Assert.That(screener.GetCloserBorder(point), Is.EqualTo(expectedBorder));
+			TestGetCloserBordertCore(x, y, expectedBorder, () => screener.topLeftRect);
 		}
 
-		[TestCase(5, 0, Border.Top)]
-		[TestCase(94, 0, Border.Top)]
-		[TestCase(5, 4, Border.Top)]
-		[TestCase(94, 4, Border.Top)]
-		[TestCase(40, 3, Border.Top)]
-		public void TestGetCloserBorder_Top(int x, int y, Border expectedBorder)
+		[TestCase(5, 0, "Top")]
+		[TestCase(94, 0, "Top")]
+		[TestCase(5, 4, "Top")]
+		[TestCase(94, 4, "Top")]
+		[TestCase(40, 3, "Top")]
+		public void TestGetCloserBorder_Top(int x, int y, string expectedBorder)
 		{
 			var point = new Point(x, y);
 			Assert.That(screener.topRect.Contains(point), Is.True);
-			Assert.That(screener.GetCloserBorder(point), Is.EqualTo(expectedBorder));
+			StringAssert.Contains(expectedBorder, screener.GetCloserBorder(point).GetType().Name);
 		}
 
-		[TestCase(95, 0, Border.Top)]
-		[TestCase(99, 0, Border.Right)]
-		[TestCase(95, 4, Border.Right)]
-		[TestCase(99, 4, Border.Right)]
-		[TestCase(97, 1, Border.Top)]
-		[TestCase(98, 2, Border.Right)]
-		[TestCase(98, 1, Border.Right)]
-		public void TestGetCloserBorder_TopRight(int x, int y, Border expectedBorder)
+		[TestCase(95, 0, "Top")]
+		[TestCase(99, 0, "Right")]
+		[TestCase(95, 4, "Right")]
+		[TestCase(99, 4, "Right")]
+		[TestCase(97, 1, "Top")]
+		[TestCase(98, 2, "Right")]
+		[TestCase(98, 1, "Right")]
+		public void TestGetCloserBorder_TopRight(int x, int y, string expectedBorder)
 		{
-			var point = new Point(x, y);
-			Assert.That(screener.topRightRect.Contains(point), Is.True);
-			Assert.That(screener.GetCloserBorder(point), Is.EqualTo(expectedBorder));
+			TestGetCloserBordertCore(x, y, expectedBorder, () => screener.topRightRect);
 		}
 
-		[TestCase(95, 5, Border.Right)]
-		[TestCase(99, 5, Border.Right)]
-		[TestCase(95, 94, Border.Right)]
-		[TestCase(99, 94, Border.Right)]
-		[TestCase(97, 40, Border.Right)]
-		public void TestGetCloserBorder_Right(int x, int y, Border expectedBorder)
+		[TestCase(95, 5, "Right")]
+		[TestCase(99, 5, "Right")]
+		[TestCase(95, 94, "Right")]
+		[TestCase(99, 94, "Right")]
+		[TestCase(97, 40, "Right")]
+		public void TestGetCloserBorder_Right(int x, int y, string expectedBorder)
 		{
-			var point = new Point(x, y);
-			Assert.That(screener.rightRect.Contains(point), Is.True);
-			Assert.That(screener.GetCloserBorder(point), Is.EqualTo(expectedBorder));
+			TestGetCloserBordertCore(x, y, expectedBorder, () => screener.rightRect);
 		}
 
-		[TestCase(95, 95, Border.Right)]
-		[TestCase(99, 95, Border.Right)]
-		[TestCase(99, 99, Border.Right)]
-		[TestCase(95, 99, Border.Bottom)]
-		[TestCase(98, 97, Border.Right)]
-		[TestCase(97, 98, Border.Bottom)]
-		[TestCase(98, 98, Border.Right)]
-		public void TestGetCloserBorder_BottomRight(int x, int y, Border expectedBorder)
+		[TestCase(95, 95, "Right")]
+		[TestCase(99, 95, "Right")]
+		[TestCase(99, 99, "Right")]
+		[TestCase(95, 99, "Bottom")]
+		[TestCase(98, 97, "Right")]
+		[TestCase(97, 98, "Bottom")]
+		[TestCase(98, 98, "Right")]
+		public void TestGetCloserBorder_BottomRight(int x, int y, string expectedBorder)
 		{
-			var point = new Point(x, y);
-			Assert.That(screener.bottomRightRect.Contains(point), Is.True);
-			Assert.That(screener.GetCloserBorder(point), Is.EqualTo(expectedBorder));
+			TestGetCloserBordertCore(x, y, expectedBorder, () => screener.bottomRightRect);
 		}
 
-		[TestCase(5, 95, Border.Bottom)]
-		[TestCase(94, 95, Border.Bottom)]
-		[TestCase(5, 99, Border.Bottom)]
-		[TestCase(94, 99, Border.Bottom)]
-		[TestCase(40, 97, Border.Bottom)]
-		public void TestGetCloserBorder_Bottom(int x, int y, Border expectedBorder)
+		[TestCase(5, 95, "Bottom")]
+		[TestCase(94, 95, "Bottom")]
+		[TestCase(5, 99, "Bottom")]
+		[TestCase(94, 99, "Bottom")]
+		[TestCase(40, 97, "Bottom")]
+		public void TestGetCloserBorder_Bottom(int x, int y, string expectedBorder)
 		{
-			var point = new Point(x, y);
-			Assert.That(screener.bottomRect.Contains(point), Is.True);
-			Assert.That(screener.GetCloserBorder(point), Is.EqualTo(expectedBorder));
+			TestGetCloserBordertCore(x, y, expectedBorder, () => screener.bottomRect);
 		}
 
-		[TestCase(0, 95, Border.Left)]
-		[TestCase(4, 95, Border.Left)]
-		[TestCase(0, 99, Border.Left)]
-		[TestCase(4, 99, Border.Bottom)]
-		[TestCase(3, 97, Border.Bottom)]
-		[TestCase(2, 96, Border.Left)]
-		[TestCase(2, 97, Border.Left)]
-		public void TestGetCloserBorder_BottomLeft(int x, int y, Border expectedBorder)
+		[TestCase(0, 95, "Left")]
+		[TestCase(4, 95, "Left")]
+		[TestCase(0, 99, "Left")]
+		[TestCase(4, 99, "Bottom")]
+		[TestCase(3, 97, "Bottom")]
+		[TestCase(2, 96, "Left")]
+		[TestCase(2, 97, "Left")]
+		public void TestGetCloserBorder_BottomLeft(int x, int y, string expectedBorder)
 		{
-			var point = new Point(x, y);
-			Assert.That(screener.bottomLeftRect.Contains(point), Is.True);
-			Assert.That(screener.GetCloserBorder(point), Is.EqualTo(expectedBorder));
+			TestGetCloserBordertCore(x, y, expectedBorder, () => screener.bottomLeftRect);
 		}
 
-		[TestCase(0, 5, Border.Left)]
-		[TestCase(4, 5, Border.Left)]
-		[TestCase(0, 94, Border.Left)]
-		[TestCase(4, 94, Border.Left)]
-		[TestCase(2, 40, Border.Left)]
-		public void TestGetCloserBordert_Left(int x, int y, Border expectedBorder)
+		[TestCase(0, 5, "Left")]
+		[TestCase(4, 5, "Left")]
+		[TestCase(0, 94, "Left")]
+		[TestCase(4, 94, "Left")]
+		[TestCase(2, 40, "Left")]
+		public void TestGetCloserBordert_Left(int x, int y, string expectedBorder)
+		{
+			TestGetCloserBordertCore(x, y, expectedBorder, () => screener.leftRect);
+		}
+
+		void TestGetCloserBordertCore(int x, int y, string expectedBorder, Func<Rectangle> rect)
 		{
 			var point = new Point(x, y);
-			Assert.That(screener.leftRect.Contains(point), Is.True);
-			Assert.That(screener.GetCloserBorder(point), Is.EqualTo(expectedBorder));
+			Assert.That(rect().Contains(point), Is.True);
+			StringAssert.Contains(expectedBorder, screener.GetCloserBorder(point).GetType().Name);
 		}
 
 		[TestCase(-10, -20)]
 		[TestCase(55, 55)]
 		public void TestGetCloserBordert(int x, int y)
 		{
-			Assert.That(screener.GetCloserBorder(new Point(x, y)), Is.EqualTo(Border.None));
+			StringAssert.Contains("None", screener.GetCloserBorder(new Point(x, y)).GetType().Name);
 		}
 
 		private Screener screener;
--- SmoothScreen/Screener.cs
+++ SmoothScreen/Screener.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 
 namespace SmoothScreen
 {
@@ -6,24 +7,24 @@ namespace SmoothScreen
 	{
 		readonly Rectangle screenBounds;
 
+		readonly Rectangle _topLeftRect;
+		readonly Rectangle _topRect;
+		readonly Rectangle _topRightRect;
+		readonly Rectangle _rightRect;
+		readonly Rectangle _bottomRightRect;
+		readonly Rectangle _bottomRect;
+		readonly Rectangle _bottomLeftRect;
+		readonly Rectangle _leftRect;
+
 #if DEBUG
-		internal readonly Rectangle topLeftRect;
-		internal readonly Rectangle topRect;
-		internal readonly Rectangle topRightRect;
-		internal readonly Rectangle rightRect;
-		internal readonly Rectangle bottomRightRect;
-		internal readonly Rectangle bottomRect;
-		internal readonly Rectangle bottomLeftRect;
-		internal readonly Rectangle leftRect;
-#else
-		readonly Rectangle topLeftRect;
-		readonly Rectangle topRect;
-		readonly Rectangle topRightRect;
-		readonly Rectangle rightRect;
-		readonly Rectangle bottomRightRect;
-		readonly Rectangle bottomRect;
-		readonly Rectangle bottomLeftRect;
-		readonly Rectangle leftRect;
+		internal Rectangle topLeftRect => _topLeftRect;
+		internal Rectangle topRect => _topRect;
+		internal Rectangle topRightRect => _topRightRect;
+		internal Rectangle rightRect => _rightRect;
+		internal Rectangle bottomRightRect => _bottomRightRect;
+		internal Rectangle bottomRect => _bottomRect;
+		internal Rectangle bottomLeftRect => _bottomLeftRect;
+		internal Rectangle leftRect => _leftRect;
 #endif
 
 		public static readonly Screener None = new Screener(new Rectangle(0, 0, 1, 1), 0);
@@ -44,40 +45,40 @@ namespace SmoothScreen
 			var innerW = w - 2 * t;
 			var innerH = h - 2 * t;
 
-			topLeftRect = new Rectangle(x, y, t, t);
-			topRect = new Rectangle(innerLeftX, y, innerW, t);
-			topRightRect = new Rectangle(innerRightX, y, t, t);
-			rightRect = new Rectangle(innerRightX, innerTopY, t, innerH);
-			bottomRightRect = new Rectangle(innerRightX, innerBottomY, t, t);
-			bottomRect = new Rectangle(innerLeftX, innerBottomY, innerW, t);
-			bottomLeftRect = new Rectangle(x, innerBottomY, t, t);
-			leftRect = new Rectangle(x, innerTopY, t, innerH);
+			_topLeftRect = new Rectangle(x, y, t, t);
+			_topRect = new Rectangle(innerLeftX, y, innerW, t);
+			_topRightRect = new Rectangle(innerRightX, y, t, t);
+			_rightRect = new Rectangle(innerRightX, innerTopY, t, innerH);
+			_bottomRightRect = new Rectangle(innerRightX, innerBottomY, t, t);
+			_bottomRect = new Rectangle(innerLeftX, innerBottomY, innerW, t);
+			_bottomLeftRect = new Rectangle(x, innerBottomY, t, t);
+			_leftRect = new Rectangle(x, innerTopY, t, innerH);
 		}
 
-		public Border GetCloserBorder(Point point)
+		public BorderBase GetCloserBorder(Point point)
 		{
 			switch (point)
 			{
 				case var pt when topRect.Contains(pt):
-					return Border.Top;
+					return topBorder;
 				case var pt when rightRect.Contains(pt):
-					return Border.Right;
+					return rightBorder;
 				case var pt when bottomRect.Contains(pt):
-					return Border.Bottom;
+					return bottomBorder;
 				case var pt when leftRect.Contains(pt):
-					return Border.Left;
+					return leftBorder;
 
 				case var pt when topLeftRect.Contains(pt):
-					return pt.X - topLeftRect.Left > pt.Y - topLeftRect.Top ? Border.Top : Border.Left;
+					return pt.X - topLeftRect.Left > pt.Y - topLeftRect.Top ? topBorder : leftBorder;
 				case var pt when topRightRect.Contains(pt):
-					return topRightRect.Right - 1 - pt.X > pt.Y - topRightRect.Top ? Border.Top : Border.Right;
+					return topRightRect.Right - 1 - pt.X > pt.Y - topRightRect.Top ? topBorder : rightBorder;
 				case var pt when bottomRightRect.Contains(pt):
-					return bottomRightRect.Right - pt.X > bottomRightRect.Bottom - pt.Y ? Border.Bottom : Border.Right;
+					return bottomRightRect.Right - pt.X > bottomRightRect.Bottom - pt.Y ? bottomBorder : rightBorder;
 				case var pt when bottomLeftRect.Contains(pt):
-					return pt.X - bottomLeftRect.Left > bottomLeftRect.Bottom - 1 - pt.Y ? Border.Bottom : Border.Left;
+					return pt.X - bottomLeftRect.Left > bottomLeftRect.Bottom - 1 - pt.Y ? bottomBorder : leftBorder;
 			}
 
-			return Border.None;
+			return noneBorder;
 		}
 
 		public bool Own(Point point) => screenBounds.Contains(point);
@@ -85,6 +86,56 @@ namespace SmoothScreen
 		public override string ToString() => this == None ? nameof(None) : screenBounds.ToString();
 
 		public override bool Equals(object obj) => obj is Screener screener && screenBounds.Equals(screener.screenBounds);
-		public override int GetHashCode() => System.HashCode.Combine(screenBounds);
+		public override int GetHashCode() => HashCode.Combine(screenBounds);
+
+		BorderBase _leftBorder;
+		BorderBase leftBorder => _leftBorder ??= new LeftBorder(this);
+
+		BorderBase _topBorder;
+		BorderBase topBorder => _topBorder ??= new TopBorder(this);
+
+		BorderBase _rightBorder;
+		BorderBase rightBorder => _rightBorder ??= new RightBorder(this);
+
+		BorderBase _bottomBorder;
+		BorderBase bottomBorder => _bottomBorder ??= new BottomBorder(this);
+
+		BorderBase _noneBorder;
+		BorderBase noneBorder => _noneBorder ??= new NoneBorder(this);
+
+		class LeftBorder : BorderBase
+		{
+			public LeftBorder(Screener screen) : base(screen)
+			{
+			}
+		}
+
+		class TopBorder : BorderBase
+		{
+			public TopBorder(Screener screen) : base(screen)
+			{
+			}
+		}
+
+		class RightBorder : BorderBase
+		{
+			public RightBorder(Screener screen) : base(screen)
+			{
+			}
+		}
+
+		class BottomBorder : BorderBase
+		{
+			public BottomBorder(Screener screen) : base(screen)
+			{
+			}
+		}
+
+		class NoneBorder : BorderBase
+		{
+			public NoneBorder(Screener screen) : base(screen)
+			{
+			}
+		}
 	}
 }
