119e87f941726447c097eb45ff61b7b2bdbe8bbb
dane.dai <Dane.dai@wisetechglobal.com>
2021-09-15 18:19:10 +0800

    wip

--- SmoothScreen/MouseState.cs
+++ SmoothScreen/MouseState.cs
@@ -4,9 +4,9 @@ namespace SmoothScreen
 {
 	// TODO: create state from pool
 	// TODO: create collection of queue for state (fixed length array)
-	readonly struct MouseState
+	struct MouseState
 	{
-		public Screener Screen { get; }
-		public Point Point { get; }
+		public Screener Screen { get; set; }
+		public Point Point { get; set; }
 	}
 }
---
+++ SmoothScreen/MouseStateQueue.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace SmoothScreen
+{
+	class MouseStateQueue
+	{
+		readonly MouseState[] items;
+		int head;
+		int tail;
+
+		public MouseStateQueue(int capacity)
+		{
+			items = new MouseState[capacity];
+			head = tail = -1;
+		}
+
+		public int Length => tail - head + (tail < head ? items.Length : 0);
+
+		public void Add(Screener screen, Point point)
+		{
+			if (Length < items.Length)
+			{
+				items[tail++] = new MouseState
+				{
+					Screen = screen,
+					Point = point
+				};
+			}
+			else
+			{
+
+			}
+		}
+	}
+}
