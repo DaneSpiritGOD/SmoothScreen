9e7f786841d6e10b9244613205970afc273b1b11
dane.dai <Dane.dai@wisetechglobal.com>
2021-09-15 12:53:32 +0800

    add test for expand screen

--- SmoothScreen.Test/ScreenerTest.cs
+++ SmoothScreen.Test/ScreenerTest.cs
@@ -110,12 +110,23 @@ namespace SmoothScreen.Test
 			StringAssert.StartsWith("None", screener.GetCloseBorder(new Point(x, y)).GetType().Name);
 		}
 
-		private Screener screener;
+		[TestCase(-10, -10)]
+		[TestCase(109, -10)]
+		[TestCase(-10, 109)]
+		[TestCase(109, 109)]
+		public void TestExpandScreen(int x, int y)
+		{
+			Assert.That(expandScreen.Own(new Point(x, y)), Is.True);
+		}
+
+		Screener screener;
+		Screener expandScreen;
 
 		[SetUp]
 		public void SetUp()
 		{
 			screener = new Screener(new Rectangle(0, 0, 100, 100), 5, 10);
+			expandScreen = screener.Expand();
 		}
 	}
 }
