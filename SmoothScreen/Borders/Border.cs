ca9ce3371739179b7153a264365c70d16aaf66ca
dane.dai <Dane.dai@wisetechglobal.com>
2021-09-16 12:47:40 +0800

    add line definition

--- SmoothScreen/Borders/Border.cs
+++ SmoothScreen/Borders/Border.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections.Generic;
+using System.Drawing;
 
 namespace SmoothScreen
 {
 	internal abstract class Border
 	{
-		private readonly Screener screen;
+		protected readonly Screener screen;
+		protected readonly Line line;
 
-		protected Border(Screener screen)
+		protected Border(Screener screen, Line line)
 		{
 			this.screen = screen;
+			this.line = line;
 		}
 
 		public override bool Equals(object obj)
@@ -18,35 +21,37 @@ namespace SmoothScreen
 
 	class LeftBorder : Border
 	{
-		public LeftBorder(Screener screen) : base(screen)
+		public LeftBorder(Screener screen, Line line) : base(screen, line)
 		{
 		}
 	}
 
 	class TopBorder : Border
 	{
-		public TopBorder(Screener screen) : base(screen)
+		public TopBorder(Screener screen, Line line) : base(screen, line)
 		{
 		}
 	}
 
 	class RightBorder : Border
 	{
-		public RightBorder(Screener screen) : base(screen)
+		public RightBorder(Screener screen, Line line) : base(screen, line)
 		{
 		}
 	}
 
 	class BottomBorder : Border
 	{
-		public BottomBorder(Screener screen) : base(screen)
+		public BottomBorder(Screener screen, Line line) : base(screen, line)
 		{
 		}
 	}
 
 	class NoneBorder : Border
 	{
-		public NoneBorder(Screener screen) : base(screen)
+		static readonly Line LineForNone = new Line(new Point(0, 0), new Point(0, 0));
+
+		public NoneBorder(Screener screen, Line line) : base(screen, LineForNone)
 		{
 		}
 	}
---
+++ SmoothScreen/Borders/Line.cs
@@ -0,0 +1,14 @@
+﻿namespace System.Drawing
+{
+	public struct Line
+	{
+		public Line(Point start, Point end)
+		{
+			Start = start;
+			End = end;
+		}
+
+		public Point Start { get; }
+		public Point End { get; }
+	}
+}
