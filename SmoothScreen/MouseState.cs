20cf894d311f003a1ab293893d1988ce6cd03755
dane.dai <Dane.dai@wisetechglobal.com>
2021-09-16 12:47:19 +0800

    remove todo

--- SmoothScreen/MouseState.cs
+++ SmoothScreen/MouseState.cs
@@ -2,8 +2,6 @@
 
 namespace SmoothScreen
 {
-	// TODO: create state from pool
-	// TODO: create collection of queue for state (fixed length array)
 	struct MouseState
 	{
 		public Screener Screen { get; set; }
