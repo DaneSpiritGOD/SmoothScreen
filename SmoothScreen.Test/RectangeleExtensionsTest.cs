3c068b0beacc0cb1b4858059a5b6a60acc699acd
dane.dai <Dane.dai@wisetechglobal.com>
2021-09-17 17:54:24 +0800

    rectangle extensions

---
+++ SmoothScreen.Test/RectangeleExtensionsTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Drawing;
+using NUnit.Framework;
+
+namespace SmoothScreen.Test
+{
+	class RectangeleExtensionsTest
+	{
+		[Test]
+		public void TestGetTop()
+		{
+			Assert.That(rect.GetTop(), Is.EqualTo(new Line(0, 0, 99, 0)));
+		}
+
+		[Test]
+		public void TestGetRight()
+		{
+			Assert.That(rect.GetRight(), Is.EqualTo(new Line(99, 0, 99, 99)));
+		}
+
+		[Test]
+		public void TestGetBottom()
+		{
+			Assert.That(rect.GetBottom(), Is.EqualTo(new Line(99, 99, 0, 99)));
+		}
+
+		[Test]
+		public void TestGetLeft()
+		{
+			Assert.That(rect.GetLeft(), Is.EqualTo(new Line(0, 99, 0, 0)));
+		}
+
+		Rectangle rect;
+
+		[SetUp]
+		public void SetUp()
+		{
+			rect = new Rectangle(0, 0, 100, 100);
+		}
+	}
+}
---
+++ SmoothScreen/RectangleExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace SmoothScreen
+{
+	static class RectangleExtensions
+	{
+		public static Line GetTop(this Rectangle rectangle) => new Line(rectangle.Location, new Point(rectangle.Right - 1, rectangle.Y));
+		public static Line GetRight(this Rectangle rectangle) => new Line(new Point(rectangle.Right - 1, rectangle.Y), new Point(rectangle.Right - 1, rectangle.Bottom - 1));
+		public static Line GetBottom(this Rectangle rectangle) => new Line(new Point(rectangle.Right - 1, rectangle.Bottom - 1), new Point(rectangle.X, rectangle.Bottom - 1));
+		public static Line GetLeft(this Rectangle rectangle) => new Line(new Point(rectangle.X, rectangle.Bottom - 1), rectangle.Location);
+	}
+}
